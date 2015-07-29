1ab8ede4e8578e73488e58510731d341eb763a51
Erika Fitzpatrick <erikafitz@Erikas-Air.home>
2015-07-29 11:29:23 -0400

    fixed links and logout button

--- ride417-android/app/src/main/java/com/ride417/myapplication/Ride417Application.java
+++ ride417-android/app/src/main/java/com/ride417/myapplication/Ride417Application.java
@@ -1,7 +1,21 @@
 package com.ride417.myapplication;
 
+import com.parse.Parse;
+import com.parse.*;
+import android.app.Application;
+
 /**
  * Created by erikafitz on 7/14/15.
  */
-public class Ride417Application {
+public class Ride417Application extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        // Required - Initialize the Parse SDK
+        Parse.initialize(this, "Rj5yrt1LSQ3mBSNxyl393ti9LTQnq5shAw3sevAO", "DrC5xwSrSiJ67QeeHgOb7K3nfZ4EkHtbrmCSNMi8");
+
+    }
 }
--- ride417-android/app/src/main/java/com/ride417/myapplication/SignUpActivity.java
+++ ride417-android/app/src/main/java/com/ride417/myapplication/SignUpActivity.java
@@ -4,4 +4,5 @@ package com.ride417.myapplication;
  * Created by erikafitz on 7/28/15.
  */
 public class SignUpActivity {
+
 }
